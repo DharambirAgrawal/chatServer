13789fed496c90a92c963e0ac837eae44e6e3a4d
Dharambirbro <dev.dharambir@gmail.com>
2024-12-11 19:22:41 -0600

    Configured tailwind and routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="checkout" element={<Checkout />} /> */}
+          {/* <Route path="/checkout/:id" element={<DynamicPage />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="bg-slate-500 text-red-500">Hello world ok</h1>
+      Home page
+    </div>
+  );
+};
+
+export default Home;
